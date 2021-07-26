896e00990d9f3c5d7def798f3adebc8fbbda9f2a
chinhtoan1 <87954466+chinhtoan1@users.noreply.github.com>
2021-07-26 09:08:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
 const CONFIG = {
-    introTitle: 'Bấy bì à!',
-    introDesc: `Địt mịa em Ai Lo ve Diu`,
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
@@ -10,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối anh em mình làm nháy nhé đừng nói nhiều.',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
